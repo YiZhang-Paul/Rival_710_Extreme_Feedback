1e11808e4fc116c26a2a799efa4363a8488c2cd4
YI ZHANG <scaccterran@gmail.com>
2019-07-03 22:06:04 -0400

    post json data to url

--- utils/jsonutils.go
+++ utils/jsonutils.go
@@ -1,9 +1,12 @@
 package utils
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
 	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 // ParseJSON parses json string into a map
@@ -30,4 +33,20 @@ func tryParseJSON(data []byte) (map[string]interface{}, bool) {
 		return parsed, false
 	}
 	return parsed, true
+}
+
+// PostJSON wraps http.Post() to reduce boilerplate code.
+// Only use this when POST response is not needed
+func PostJSON(url string, data interface{}) bool {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	_, err = http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Println(err)
+	}
+	return err == nil
 }
