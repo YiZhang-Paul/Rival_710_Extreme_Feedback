c3f450764196b8b3be08d5cfa7fabf0e28753f9c
YI ZHANG <scaccterran@gmail.com>
2019-07-04 19:12:47 -0400

    display static/shift text

--- controls/rival710/screencontrol.go
+++ controls/rival710/screencontrol.go
@@ -2,6 +2,7 @@ package rival710
 
 import (
 	"strconv"
+	"time"
 
 	"github.com/yi-zhang/rival-710-extreme-feedback/controls"
 	"github.com/yi-zhang/rival-710-extreme-feedback/utils"
@@ -17,6 +18,41 @@ type ScreenControl struct {
 func (sc ScreenControl) bindText(meta controls.ScreenMeta, event int) bool {
 	data := sc.newBindMeta(meta, strconv.Itoa(event))
 	return utils.PostJSON(meta.BindAPI, data)
+}
+
+func (sc ScreenControl) applyText(meta controls.ScreenMeta, text string, event int) bool {
+	data := controls.NewTriggerMeta(meta.Game, strconv.Itoa(event), text)
+	return utils.PostJSON(meta.TriggerAPI, data)
+}
+
+func (sc *ScreenControl) applyStatic(meta controls.ScreenMeta, event int) bool {
+	sc.event = 0
+	return sc.applyText(meta, meta.Content[0], event)
+}
+
+func (sc *ScreenControl) applyShift(meta controls.ScreenMeta, interval, event int) bool {
+	sc.contents = meta.Content
+	if sc.event == event {
+		return false
+	}
+	var (
+		loop         func(int)
+		milliseconds = time.Duration(interval) * time.Microsecond
+	)
+	loop = func(index int) {
+		if sc.event != event {
+			return
+		}
+		content := sc.contents[utils.NextIndex(sc.contents, index)]
+		sc.applyText(meta, content, event)
+		select {
+		case <-time.After(milliseconds):
+			loop(utils.NextIndex(sc.contents, index))
+		}
+	}
+	sc.event = event
+	go loop(0)
+	return true
 }
 
 func (sc ScreenControl) newBindMeta(screenMeta controls.ScreenMeta, event string) controls.BindMeta {
