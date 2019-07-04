f319627970194dbf2a62b352f112d3351f213682
YI ZHANG <scaccterran@gmail.com>
2019-07-03 20:37:02 -0400

    subscribe to ci/cd poller service

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	_ "github.com/joho/godotenv/autoload"
+)
+
+var (
+	port       = os.Getenv("PORT")
+	host       = os.Getenv("SELF_HOST")
+	targetHost = os.Getenv("TARGET_HOST")
+)
+
+func main() {
+	var (
+		target   = fmt.Sprintf("%s/subscription", targetHost)
+		callback = fmt.Sprintf("%s:%s/notify", host, port)
+	)
+	if id, err := subscribe(target, callback); err != nil {
+		log.Fatalf("Subscription failed at: %s. Error: %v\n", target, err)
+	} else {
+		log.Printf("Subscription created. ID: %s\n", id)
+	}
 }
---
+++ subscription.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+func subscribe(target, callback string) (string, error) {
+	payload := map[string]interface{}{"callbackUrl": callback}
+	data, err := json.Marshal(payload)
+	if err != nil {
+		return "", err
+	}
+
+	res, err := http.Post(target, "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	if id, err := ioutil.ReadAll(res.Body); err != nil {
+		return "", err
+	} else {
+		return string(id), nil
+	}
+}
