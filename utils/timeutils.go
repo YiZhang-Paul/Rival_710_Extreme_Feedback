62e627ebe402d6ba0224055d0063756d8e37c79c
YI ZHANG <scaccterran@gmail.com>
2019-07-04 19:28:23 -0400

    time utilities

---
+++ utils/timeutils.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// FormatTime formats seconds into time string 'HH:mm:ss'
+func FormatTime(seconds int) string {
+	toTimeString := func(value int) string {
+		if value < 10 {
+			return fmt.Sprintf("0%d", value)
+		}
+		return strconv.Itoa(value)
+	}
+	var (
+		hour   = toTimeString(seconds / 3600)
+		minute = toTimeString(seconds % 3600 / 60)
+		second = toTimeString(seconds % 60)
+	)
+	return fmt.Sprintf("%s:%s:%s", hour, minute, second)
+}
