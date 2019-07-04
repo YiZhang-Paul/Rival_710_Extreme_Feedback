f49cf6cf55e0cbcda607e977463f59d12cdefbf2
YI ZHANG <scaccterran@gmail.com>
2019-07-03 21:54:02 -0400

    parse json from file

--- utils/jsonutils.go
+++ utils/jsonutils.go
@@ -8,9 +8,25 @@ import (
 
 // ParseJSON parses json string into a map
 func ParseJSON(input io.Reader) (map[string]interface{}, bool) {
-	parsed := make(map[string]interface{})
 	content, err := ioutil.ReadAll(input)
-	if err != nil || json.Unmarshal(content, &parsed) != nil {
+	if err != nil {
+		return make(map[string]interface{}), false
+	}
+	return tryParseJSON(content)
+}
+
+// ParseJSONFile takes full path of a json file and parse it into a map
+func ParseJSONFile(path string) (map[string]interface{}, bool) {
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		return make(map[string]interface{}), false
+	}
+	return tryParseJSON(file)
+}
+
+func tryParseJSON(data []byte) (map[string]interface{}, bool) {
+	parsed := make(map[string]interface{})
+	if json.Unmarshal(data, &parsed) != nil {
 		return parsed, false
 	}
 	return parsed, true
